d05f9b06071769483e63c64bb8eae061a4de3f91
Beto Cortés <roberto.cortes13233@gmail.com>
2021-03-12 15:27:55 -0600

    [add] good answers and kind of responses

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const response = require('./network/response')
 const router = express.Router()
 
 let app = express()
@@ -9,16 +10,27 @@ app.use(router)
 router.get('/messages', (req, res) => {
   console.log(req.headers)
   res.header({
-    "custom-proprty": "custom value"
+    "custom-property": "custom value"
   })
-  res.send('Message list')
-  console.log(req.headers)
+  //res.send('Message list')
+
+  response.success(req, res, 'Lista de mensajes', 200)
+  /* console.log(req.headers) */
 })
 
 router.post('/messages', (req, res) => {
-  console.log(req.body)
-  console.log(req.query)
-  res.send(`message ${req.body.text} added`)
+  /*   console.log(req.body)
+    console.log(req.query) */
+  /* res.status(201).send({
+    error: '',
+    body: `${req.body} created`
+  }) */
+
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Internal Server Error', 500)
+  } else {
+    response.success(req, res, 'Message created', 201)
+  }
 })
 
 /* 
